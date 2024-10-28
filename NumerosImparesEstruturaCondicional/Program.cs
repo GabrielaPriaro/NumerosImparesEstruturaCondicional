85f33079dbbd8d00379dbe5883f3108ebcf766f9
Gabriela <gabriela.priaro@gmail.com>
2024-10-28 17:27:29 +0000

    Implementaçao para mostrar os numeros impares até o numero escolhido.

--- NumerosImparesEstruturaCondicional/Program.cs
+++ NumerosImparesEstruturaCondicional/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace NumerosImparesEstruturaCondicional
+﻿//obs: Utilizando Estrutura Condicional For:
+//Problema "sequencia_impares" (adaptado de URI 1067)
+//Leia um valor inteiro X. Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso. 
+
+namespace NumerosImparesEstruturaCondicional
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x;
+
+            Console.Write("Digite o valor de X: ");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++) 
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
