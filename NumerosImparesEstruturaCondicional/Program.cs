9b8462f2a425e931219e6865e07413c4247b68e0
Gabriela <gabriela.priaro@gmail.com>
2024-10-28 17:14:16 +0000

    Adicionar arquivos de projeto.

---
+++ NumerosImparesEstruturaCondicional/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NumerosImparesEstruturaCondicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
